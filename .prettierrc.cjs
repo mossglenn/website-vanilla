71df799f82a35188145cefa53194b5174085b2b0
Amos Glenn <41129047+mossglenn@users.noreply.github.com>
2025-05-08 14:16:41 -0400

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,29 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: [
+    'prettier-plugin-astro',
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss' // must be last
+  ],
+  pluginSearchDirs: false,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ],
+  importOrder: ['<TYPES>', '<TYPES>^[.]', '', '<THIRD_PARTY_MODULES>', '', '^[@]/', '', '^[.]']
+};
